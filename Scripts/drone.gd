a6e3375f7f380cea03254c6705449b78d3b968a2
kyzfrintin <kyle.harmo@live.co.uk>
2018-04-26 18:31:58 +0100

    AutoCommit at 26/04/2018 18:31:58

--- Scripts/drone.gd
+++ Scripts/drone.gd
@@ -46,6 +46,7 @@ func approach():
 	
 func attack():
 	look_at(player.pos)
+	att_snd.play()
 	var att_vec = att_ray.cast_to.rotated(rotation)
 	att_tween.interpolate_property(self, 'position', position, att_vec, 0.5/strength, Tween.TRANS_EXPO, Tween.EASE_IN)
 	att_tween.start()
